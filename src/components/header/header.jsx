ed42b5540124ccf1ec5c3f66b4fe72bdd25c4895
Juthy <69724408+Juthy07@users.noreply.github.com>
2022-10-26 17:08:44 +0530

    added header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <div className="flex">
+      <div className="flex">
+        <img src="/logo.png" alt="" />
+      </div>
+      <div className="flex">
+        <span>Home</span>
+        <span>Contact Us</span>
+        <span>Reservation</span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/home.jsx
+++ src/home.jsx
@@ -1,5 +1,11 @@
+import Header from "./components/header/header";
+
 function Home() {
-  return <h1>Welcome to Tower Hill!</h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default Home;
